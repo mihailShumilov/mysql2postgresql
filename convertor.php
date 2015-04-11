39944c79e307bab6b542ec6a06ac8db2601c2f43
Mihail Shumilov <mschumilow@gmail.com>
2015-04-11 20:06:21 +0300

    fix notice

--- convertor.php
+++ convertor.php
@@ -10,9 +10,9 @@
 
     $options = getopt( "i:o:b::", array( "input-file:", "output-file:", "batch-count::" ) );
 
-    $iFilePath  = $options["i"] ? $options["i"] : $options["input-file"];
-    $oFilePath  = $options["o"] ? $options["o"] : $options["output-file"];
-    $batchCount = $options["b"] ? $options["b"] : ( $options["batch-count"] ? $options["batch-count"] : false );
+    $iFilePath  = isset( $options["i"] ) ? $options["i"] : $options["input-file"];
+    $oFilePath  = isset( $options["o"] ) ? $options["o"] : $options["output-file"];
+    $batchCount = isset( $options["b"] ) ? $options["b"] : ( isset( $options["batch-count"] ) ? $options["batch-count"] : false );
 
     $conv = new Convertor( array(
         "iFileName"  => $iFilePath,
